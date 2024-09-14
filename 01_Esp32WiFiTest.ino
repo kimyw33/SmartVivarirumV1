ba9c03b15a7a42f0c037e1534924b5ac7f02bc08
kimyw33 <kimyw33@gmail.com>
2024-09-14 21:26:30 +0900

    간결하게 바꿈

--- 01_Esp32WiFiTest.ino
+++ 01_Esp32WiFiTest.ino
@@ -1,30 +1,19 @@
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID  "TMPL6xQ5btb2J"   // Blynk template ID
+#define BLYNK_AUTH_TOKEN   "R0f9Z-7VEkCk3TUDCu5S-vjX-2Ug5ZCh"  // Blynk auth token
 
-/* Fill in information from Blynk Device Info here */
-#define BLYNK_TEMPLATE_ID           "TMPL6xQ5btb2J"
-#define BLYNK_TEMPLATE_NAME         "SmartFarmTest00"
-#define BLYNK_AUTH_TOKEN            "R0f9Z-7VEkCk3TUDCu5S-vjX-2Ug5ZCh"
+#include <WiFi.h>               // WiFi library for ESP32
+#include <BlynkSimpleEsp32.h>    // Blynk library for ESP32
 
-
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "4층 메이커실";
-char pass[] = "ksm1234567";
+// WiFi credentials
+char ssid[] = "4층 메이커실";    
+char pass[] = "ksm1234567";      
 
 void setup()
 {
-  // Debug console
-  Serial.begin(9600);
-
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);  // Connect to WiFi and Blynk
 }
 
 void loop()
 {
-  Blynk.run();
+  Blynk.run();  // Keep Blynk connection active
 }
