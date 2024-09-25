85083ba82f0373e90a5346eb69bd706df401c5b3
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2024-09-25 12:14:53 +0900

    리드미파일 정정

--- 03_DhtSensor.ino
+++ 03_DhtSensor.ino
@@ -3,7 +3,7 @@
 #define DHTPIN 14  // DHT 센서를 연결한 핀 번호 (GPIO 14)
 #define DHTTYPE DHT11  // DHT 센서의 종류 (DHT11)
 
-DHT dht(DHTPIN, DHTTYPE);  // DHT 객체 생성, 핀 번호와 센서 타입을 설정
+DHT dht(DHTPIN, DHTTYPE);  // DHT 객체 생성, 위에서 정의한 핀 번호와 센서 타입을 매개변수로 전달
 
 void setup() {
   Serial.begin(9600);  // 시리얼 통신 시작 (9600 baud rate)
@@ -17,7 +17,7 @@ void loop() {
   
   Serial.print("Temp: ");  // 온도 출력
   Serial.print(t);  // 읽어들인 온도 값 출력
-  Serial.print(", ");  
+  Serial.print(", ");
   Serial.print("Humi: ");  // 습도 출력
   Serial.println(h);  // 읽어들인 습도 값 출력
 }
