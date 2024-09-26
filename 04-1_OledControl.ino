8f619da08863433d22ad8cd00ff2c1f442dd766a
김영우 <youngwookim@gim-yeong-uui-MacBookPro.local>
2024-09-26 10:33:21 +0900

    리드미파일 정정

--- 04-1_OledControl.ino
+++ 04-1_OledControl.ino
@@ -44,15 +44,15 @@ void loop() {
   int Cds_value = analogRead(Cds);  // 조도 센서로부터 아날로그 값을 읽어들임
   String brightness;
   if (Cds_value < 820) {
-    brightness = "So Dark";  // 조도 값이 매우 낮으면 매우 어두운 상태
+    brightness = "So Dark";  // 조도 값이 매우 낮으면 "So Dark"(매우 어두움)
   } else if (Cds_value < 1630) {
-    brightness = "Dark";  // 조도 값이 낮으면 어두운 상태
+    brightness = "Dark";  // 조도 값이 낮으면 "Dark"(어두움)
   } else if (Cds_value < 2450) {
-    brightness = "Normal";  // 조도 값이 중간이면 보통 상태
+    brightness = "Normal";  // 조도 값이 중간이면 "Normal"(보통)
   } else if (Cds_value < 3270) {
-    brightness = "Bright";  // 조도 값이 높으면 밝은 상태
+    brightness = "Bright";  // 조도 값이 높으면 "Bright"(밝음)
   } else {
-    brightness = "So Bright";  // 조도 값이 매우 높으면 매우 밝은 상태
+    brightness = "So Bright";  // 조도 값이 매우 높으면 "So Bright"매우 밝음
   }
 
   // 디스플레이에 온도, 습도, 조도 값 출력
